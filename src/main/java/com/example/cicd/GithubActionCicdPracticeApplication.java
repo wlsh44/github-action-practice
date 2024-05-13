f107de5b44ceaac4b179bf20a09c2a08267ccbd4
wlsh44 <k87913j@gmail.com>
2024-05-13 09:12:26 +0900

    init project

---
+++ src/main/java/com/example/cicd/GithubActionCicdPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionCicdPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionCicdPracticeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/cicd/MyController.java
@@ -0,0 +1,17 @@
+package com.example.cicd;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @Value("branch")
+    private String branch;
+
+    @GetMapping("")
+    public String branch() {
+        return branch;
+    }
+}
